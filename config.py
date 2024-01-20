ea6f719bb4325cc6c371bf479edc4ff3153d1521
mahmoud-gg <136716364+mahmoud-gg@users.noreply.github.com>
2024-01-20 21:46:21 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ import os
 APP_ID = os.environ.get("APP_ID", "29167512")
 APP_HASH = os.environ.get("APP_HASH", "4c9ec8f8fc4989491e71fcff719008bd")
 SESSION = os.environ.get("SESSION")
-ha313so = TelegramClient(StringSession(SESSION), APP_ID, APP_HASH)
-ha313so.start()
+ma515 = TelegramClient(StringSession(SESSION), APP_ID, APP_HASH)
+ma515.start()
